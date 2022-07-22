7d5cc2ce1292a15bea7a2bc375bb0f271de65787
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-07-22 17:26:47 +0600

    slice method

---
+++ slice.js
@@ -0,0 +1,13 @@
+// Array.prototype.slice()
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let name = 'Bangladesh';
+
+let show = name.slice(6)
+
+let result = numbers.slice(4,8);
+console.log(result);
+console.log(show);
+
+console.log(numbers);
+console.log(name);
