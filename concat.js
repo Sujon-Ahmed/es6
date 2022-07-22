d0bda551182a56ee381e84202ec3e7cc612befed
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-07-22 18:12:40 +0600

    concat() method

---
+++ concat.js
@@ -0,0 +1,11 @@
+// Array.prototype.concat()
+
+// concat() method can't change main array it's return new array
+
+let firstName = 'Sujon';
+let middleName = ' Ahmed ';
+let lastName = 'Riman';
+
+let name = firstName.concat(middleName, lastName);
+
+console.log(name);
