6216ab24b35705cd820d106f6deaba4a016c0be9
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-07-22 20:07:06 +0600

    push, pop, map method done

---
+++ map.js
@@ -0,0 +1,8 @@
+// Array.prototype.map()
+
+const numbers = [1, 2, 3, 4, 5];
+
+let result = numbers.map((num) => num * 2);
+
+console.log(numbers);
+console.log(result);
---
+++ push.js
@@ -0,0 +1,9 @@
+// Array.prototype.push()
+// push() method change main array
+const numbers = [1, 2, 3, 4];
+
+numbers.push(5); // push() method add new element in array form last
+
+numbers.pop(2); // pop() method remove element in array from last
+
+console.log(numbers);
