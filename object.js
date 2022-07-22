e8f4443a13b94d900be6a3655374fb075c62f26b
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-07-22 21:01:30 +0600

    important obj method

---
+++ object.js
@@ -0,0 +1,16 @@
+// Important Object Method
+
+const myObj = {
+    name : 'JavaScript',
+    estd : '1995',
+    founder : 'Brendan Eich',
+    ranking : 1
+}
+
+let keys = Object.keys(myObj);  // return object keys
+let values = Object.values(myObj);  // return object values
+let entries = Object.entries(myObj);    // return object key:values pair entries
+
+// console.log(keys); 
+// console.log(values);
+console.log(entries);
