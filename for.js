3b6194b79cd29f20b8cc738eda88ed04e8d2180d
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-07-22 20:52:17 +0600

    for, for in , for of

---
+++ for.js
@@ -0,0 +1,28 @@
+// for loop, for in, for of
+// for loop
+
+const message = 'I love Bangladesh';
+
+const numbers = [1, 2, 3, 4, 5];
+
+const programmingLanguage = {
+    name : 'JavaScript',
+    estd : '1995',
+    founder : 'Brendan Eich'
+};
+
+// for loop
+// for (let i = 1; i <= 3; i++) {
+//     console.log(i);
+// }
+
+// for in
+// for (property in programmingLanguage) {
+//     console.log(property);
+// }
+
+// for of
+for (element of numbers) {
+    console.log(element);
+}
+
