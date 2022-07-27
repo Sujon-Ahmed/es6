c4305a15d1ecbf7942123a7a58e08a9abde7539e
Sujon Ahmed <sujonahmed5284@gmail.com>
2022-07-27 23:01:24 +0600

    classes inheritance

--- classes.js
+++ classes.js
@@ -2,11 +2,19 @@ class Car {
     constructor(name) {
         this.brand = name;
     }
-
     present() {
         return `I have a ${this.brand}`;
     }
 }
+class Model extends Car {
+    constructor(name, model) {
+        super(name);
+        this.model = model;
+    }
+    show() {
+        return `${this.present()}, it is a ${this.model}`;
+    }
+} 
 
-const myCar = new Car('BMW');
-console.log(myCar.present());
+const myCar = new Model('Ford','Mustang');
+console.log(myCar.show());
